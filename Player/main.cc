7475d2059b151cc42272c45387ecfbeb12ccf4a8
Par Bhatia <parshantx@hotmail.com>
2018-03-28 00:21:29 -0400

    started creating command interpreter

---
+++ Player/main.cc
@@ -0,0 +1,15 @@
+#include <shit>
+
+int main() {
+    //Command Interpreter
+    string s;
+    cin << s;
+    while(cin << s) {
+        if (s == "move") {
+            do;
+        }
+        if (s == "resign") {
+            do;
+        }
+    }
+}
