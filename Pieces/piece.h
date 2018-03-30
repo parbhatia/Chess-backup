74d75531a6a8c2bb0c1a97d2cd22e7b947613a59
Berdem <b2yildiz@uwaterloo.ca>
2018-03-30 18:59:50 -0400

    Update piece.h

--- Pieces/piece.h
+++ Pieces/piece.h
@@ -14,5 +14,5 @@ class Piece {
 	virtual bool IsLegal(int oldPosX, int oldPosY, int newPosX, int newPosY) = 0;
 	virtual vector<vector<int>> possiblemoves() = 0;
 	virtual Piece(std::string c, int posx, int posy, bool hasMoved, bool enPassant);
-	virtual ~Piece();
+	~Piece();
 };
