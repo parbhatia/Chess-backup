d921f1546228e6c578865f0fcfacfa2ddbaeaab2
Berdem <b2yildiz@uwaterloo.ca>
2018-03-30 18:54:19 -0400

    Update piece.cc

--- Pieces/piece.cc
+++ Pieces/piece.cc
@@ -20,7 +20,8 @@ void Piece::setPassant(bool val) {
 	enPassant = val;
 }
 	
-Piece::Piece(std::string c, int posx, int posy, bool hasMoved = false, bool enPassant = false): color{c}, posx{posX}, posy{posY}, hasMoved{hasMoved}, enPassant{enPassant} {}
+Piece::Piece(std::string c, int posx, int posy, bool hasMoved = false, bool enPassant = false): color{c}, posx{posX}, 
+	posy{posY}, hasMoved{hasMoved}, enPassant{enPassant} {}
 
 Piece::Piece~() {}
 
