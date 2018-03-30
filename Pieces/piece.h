8ae26e1d160d3d4ed0d48c3f397fab6af11a06cf
Berdem <b2yildiz@uwaterloo.ca>
2018-03-30 18:02:11 -0400

    Update piece.h

--- Pieces/piece.h
+++ Pieces/piece.h
@@ -1,19 +1,19 @@
-#include <bool>
 #include <string>
 
 class Piece {
 	const string color;
 	int posX;
 	int posY;
+	bool canJump;
 	bool enPassant;
 	bool hasMoved;
-	bool canJump;
 	public:
 	void setMoved(bool val);
-	void setPassant(bool val);
 	void updatePos(string newPos);
+	virtual void setPassant(bool val);
 	virtual bool canCastle();
-	virtual bool moveIsLegal(string oldPos, string newPos) = 0;
-	virtual Piece();
+	virtual bool IsLegal(int oldPosX, int oldPosY, int newPosX, int newPosY) = 0;
+	virtual vector<vector<int>> possiblemoves() = 0;
+	virtual Piece(string c, int posx, int posy, bool canJump, bool enPassant = false, bool hasMoves = false);
 	virtual ~Piece() {}
 };
