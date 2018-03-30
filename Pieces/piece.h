0a7bd59c4b3b6164d557dbf99fc5f100f76d0e57
Berdem <b2yildiz@uwaterloo.ca>
2018-03-30 18:48:20 -0400

    Update piece.h

--- Pieces/piece.h
+++ Pieces/piece.h
@@ -4,16 +4,15 @@ class Piece {
 	const std::string color;
 	int posX;
 	int posY;
-	bool canJump;
 	bool hasMoved;
 	bool enPassant;
 	public:
-	void setMoved(bool val);
-	void updatePos(std::string newPos);
+	void setMoved();
+	void updatePos(int newPosX, int newPosY);
+	virtual bool canJump();
 	virtual void setPassant(bool val);
-	virtual bool canCastle();
 	virtual bool IsLegal(int oldPosX, int oldPosY, int newPosX, int newPosY) = 0;
 	virtual vector<vector<int>> possiblemoves() = 0;
-	virtual Piece(std::string c, int posx, int posy, bool canJump, bool hasMoves = false, bool enPassant = false);
+	virtual Piece(std::string c, int posx, int posy, bool hasMoved = false, bool enPassant = false);
 	virtual ~Piece();
 };
