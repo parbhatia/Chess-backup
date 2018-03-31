a34abbc43a5a63ca798d38df6200d3a24629071c
Berdem <b2yildiz@uwaterloo.ca>
2018-03-31 17:49:12 -0400

    Update piece.h

--- Pieces/piece.h
+++ Pieces/piece.h
@@ -12,7 +12,7 @@ class Piece {
 	virtual bool canJump();
 	virtual void setPassant(bool val);
 	virtual bool IsLegal(Pos newPos, vector <vector<Piece*>>* pieces) = 0;
-	virtual vector<pos> possiblemoves(vector <vector<Piece*>>* pieces) = 0;
+	virtual vector<pos>* possiblemoves(vector <vector<Piece*>>* pieces) = 0;
 	virtual Piece(Color c, pos Pos, bool hasMoved, bool enPassant);
 	//No need a destructor since there is no heap allocated field and default dest is enough
 };
