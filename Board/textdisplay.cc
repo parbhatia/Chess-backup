d9448311d410069013fb9951c30a53a30f4e95d3
Par Bhatia <parshantx@hotmail.com>
2018-03-31 01:32:06 -0400

    added textdisplay files

--- Board/textdisplay.cc
+++ Board/textdisplay.cc
@@ -1,16 +1,19 @@
-#ifndef TEXT_DISPLAY
-#define TEXT_DISPLAY
-class Piece;
+#include "textdisplay.h"
+#include <iostream>
+#include <string>
+using namespace std;
 
 class TextDisplay {
     std::vector<std::vector<char>> theDisplay;
 	public:
 	void insert (string pos, piece);
 	void remove (string pos);
-	//void setup_all(); //if board calls this every time it adds a piece, dont need this
+	/*void setup_all() {} //if board calls this every time it adds a piece, dont need this
+
 	TextDisplay();
 };
 
 ostream operator<<(ostream &os, const TextDisplay &b);
 
-#endif
+}
+
