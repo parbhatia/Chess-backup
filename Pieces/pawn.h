a020ad04d0853088ea983e62fa7f00c38adcbc54
Berdem <b2yildiz@uwaterloo.ca>
2018-03-31 19:25:55 -0400

    Update pawn.h

--- Pieces/pawn.h
+++ Pieces/pawn.h
@@ -2,7 +2,7 @@
 
 class Pawn: public Piece {
 	void setPassant(bool val) override;
-	bool IsLegal(pos newPos) override;
-	vector<pos> possiblemoves() override;
-	Pawn(Color c, pos Pos, bool hasMoved, bool enPassant);
+	bool IsLegal(Pos newPos, vector <vector<Piece*>>* pieces) override;
+	vector<pos>* possiblemoves(vector <vector<Piece*>>* pieces) override;
+	Pawn(Color c, Pos Pos, bool hasMoved, bool enPassant);
 };
