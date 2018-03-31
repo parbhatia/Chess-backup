eee8913b626c9d4db66d76d5e53bacd3743abde5
Berdem <b2yildiz@uwaterloo.ca>
2018-03-31 12:37:42 -0400

    Update pawn.cc

--- Pieces/pawn.cc
+++ Pieces/pawn.cc
@@ -1,3 +1,11 @@
 #include "pawn.h"
 
 void Pawn::setPassant(bool val) {
+  enPassant = val;
+}
+
+bool Pawn::isLegal(int oldPosX, int oldPosY, int newPosX, int newPosY) {} //IMPLEMENT LATER
+
+
+vector<vector<int>> Piece::possiblemoves() {
+  vector<vector<int>> vec = nullptr;
