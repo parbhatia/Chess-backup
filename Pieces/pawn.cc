06c020a017171bbafc9cb6c10466c55ace85f549
Berdem <b2yildiz@uwaterloo.ca>
2018-03-31 18:48:03 -0400

    Update pawn.cc

--- Pieces/pawn.cc
+++ Pieces/pawn.cc
@@ -1,23 +1,46 @@
 #include "pawn.h"
+#include "pos.h"
+using namespace std;
 
 void Pawn::setPassant(bool val) {
   enPassant = val;
 }
 
-bool Pawn::isLegal(int newPosX, int newPosY) {} //IMPLEMENT LATER
+bool Pawn::IsLegal(pos newPos, vector <vector<Piece*>>* pieces) {
+  if(color == Black) {
+    if(((Pos.col == newPos.col) && (Pos.row == (newPos.row - 1)) && (pieces[newPos.row][newPos.col] == nullptr)) ||  //1 forward
+       ((hasMoved == false) && (Pos.col == newPos.col) && (Pos.row == (newPos.row - 2)) &&   //2 forward 
+        (pieces[newPos.row][newPos.col] == nullptr) && (pieces[newPos.row - 1][newPos.col] == nullptr)) ||
+       (((Pos.col == (newPos.col + 1)) || (Pos.col == (newPos.col - 1))) && (Pos.row == (newPos.row - 1)) &&   //1 diagnol
+        (pieces[newPos.row][newPos.col] != nullptr) && (pieces[newPos.row][newPos.col].color == White))) {
+      return true;
+    } else {
+      return false;
+    }
+  } else if (color == White) {
+    if(((Pos.col == newPos.col) && (Pos.row == (newPos.row + 1)) && (pieces[newPos.row][newPos.col] == nullptr)) ||  //1 forward
+       ((hasMoved == false) && (Pos.col == newPos.col) && (Pos.row == (newPos.row + 2)) &&   //2 forward 
+        (pieces[newPos.row][newPos.col] == nullptr) && (pieces[newPos.row + 1][newPos.col] == nullptr)) ||
+       (((Pos.col == (newPos.col + 1)) || (Pos.col == (newPos.col - 1))) && (Pos.row == (newPos.row + 1)) &&   //1 diagnol
+        (pieces[newPos.row][newPos.col] != nullptr) && (pieces[newPos.row][newPos.col].color == Black))) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
 
-
-vector<vector<int>> Piece::possiblemoves() {
-  vector<vector<int>> vec = nullptr;
+      
+       
+vector<pos>* Pawn::possiblemoves() {
+  vector<pos> vec = nullptr;
   
   if(color == Black) {
     if(hasMoved == false) {   //2 forward move
-      vector<int> v1 = nullptr;
-      v1.push_back(posY + 2);
-      v1.push_back(posX);
+      
       vec.push_back(v1);
     }
-    vector<int> v2 = nullptr;
+    vector<int> v2 = nullptr;   //1 forward move
     v2.push_back(
     } else if(color == White) {
         
