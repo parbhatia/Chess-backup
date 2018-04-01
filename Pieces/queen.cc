5eef8418a7a0f23b952bfb4cbfa84b7b45a7db68
Berdem <b2yildiz@uwaterloo.ca>
2018-04-01 17:46:28 -0400

    Update queen.cc

--- Pieces/queen.cc
+++ Pieces/queen.cc
@@ -75,3 +75,83 @@ bool Queen::IsLegal(Pos newPos, vector <vector<Piece*>>* pieces) {  //Assuming n
     return false;
   }
 }
+
+vector<Pos> Bishop::getPossibleMoves(vector <vector<Piece*>>* pieces) { //We need to check destination cell if it's empty or enemy
+    vector<Pos> vec = nullptr;
+
+    for(int i = Pos.col + 1; i <= 7; ++i) { //check right
+    if(((pieces[Pos.row][i] == nullptr) || (pieces[Pos.row][i].getColor() != color)) &&  //destionation cell condition
+       (IsLegal({Pos.row, i}, vector <vector<Piece*>>* pieces) == true)) {
+      vec.push_back = {i,Pos.col};
+    } else {
+      break;
+    }
+  }
+
+  for(int i = Pos.col - 1; i >= 0; --i) { //check left
+    if(((pieces[Pos.row][i] == nullptr) || (pieces[Pos.row][i].getColor != color)) &&  //destionation cell condition
+       (IsLegal({Pos.row, i}, vector <vector<Piece*>>* pieces) == true)) {
+      vec.push_back = {i,Pos.col};
+    } else {
+      break;
+    }
+  }
+
+  for(int i = Pos.row + 1; i <= 7; ++i) { //check bottom
+    if(((pieces[i][Pos.col] == nullptr) || (pieces[i][Pos.col].getColor != color)) &&  //destionation cell condition
+       (IsLegal({i, Pos.col}, vector <vector<Piece*>>* pieces) == true)) {
+      vec.push_back = {i, Pos.col};
+    } else {
+      break;
+    }
+  }
+
+  for(int i = Pos.row - 1; i >= 0; --i) { //check top
+    if(((pieces[i][Pos.col] == nullptr) || (pieces[i][Pos.col].getColor != color)) &&  //destionation cell condition
+       (IsLegal({i, Pos.col}, vector <vector<Piece*>>* pieces) == true)) {
+      vec.push_back = {i, Pos.col};
+    } else {
+      break;
+    }
+  }
+
+  for(int i = Pos.row + 1, int y = Pos.col + 1; (i <= 7) && (y <= 7); ++i, ++y) { //check right-bottom
+    if(((pieces[i][y] == nullptr) || (pieces[i][y].color != color)) &&  //destionation cell condition
+       (IsLegal({i, y}, vector <vector<Piece*>>* pieces) == true)) {
+      vec.push_back = {i,y};
+    } else {
+      break;
+    }
+  }
+  
+  for(int i = Pos.row + 1, int y = Pos.col - 1; (i <= 7) && (y >= 0); ++i, --y) { //check left-bottom
+    if(((pieces[i][y] == nullptr) || (pieces[i][y].getColor != color)) &&  //destionation cell condition
+       (IsLegal({i, y}, vector <vector<Piece*>>* pieces) == true)) {
+      vec.push_back = {i,y};
+    } else {
+      break;
+    }
+  }
+  
+  for(int i = Pos.row - 1, int y = Pos.col + 1; (i >= 0) && (y <= 7); --i, ++y) { //check right-top
+    if(((pieces[i][y] == nullptr) || (pieces[i][y].getColor != color)) &&  //destionation cell condition
+       (IsLegal({i, y}, vector <vector<Piece*>>* pieces) == true)) {
+      vec.push_back = {i,y};
+    } else {
+      break;
+    }
+  }
+  
+  for(int i = Pos.row - 1, int y = Pos.col - 1; (i >= 0) && (y >= 0); --i, --y) { //check left-top
+    if(((pieces[i][y] == nullptr) || (pieces[i][y].getColor != color)) &&  //destionation cell condition
+       (IsLegal({i, y}, vector <vector<Piece*>>* pieces) == true)) {
+      vec.push_back = {i,y};
+    } else {
+      break;
+    }
+  }
+
+  return vec;
+}
+
+Queen:Queen(Color c, Pos Pos, bool hasMoved = false, bool enPassant = false): Piece(c , Pos) {}
