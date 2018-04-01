3cecdad73fa36059d696a70bfc07ce473c8d53cd
Berdem <b2yildiz@uwaterloo.ca>
2018-04-01 01:06:27 -0400

    Update rook.h

--- Pieces/rook.h
+++ Pieces/rook.h
@@ -1,8 +1,14 @@
+#ifndef ROOK_H
+#define ROOK_H
+
+
 #include "piece.h"
 #include "pos.h"
 
 class Rook: public Piece {
 	bool IsLegal(Pos newPos, vector <vector<Piece*>>* pieces) override;
-	vector<pos>* possiblemoves(vector <vector<Piece*>>* pieces) override;
+	vector<pos> getPossibleMoves(vector <vector<Piece*>>* pieces) override;
 	Rook(Color c, Pos Pos, bool hasMoved, bool enPassant);
 };
+
+#endif
