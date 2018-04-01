0e4b404669132b4b62add1e9fcb72642b429f829
Javidan <cavidan.hasanzade98@gmail.com>
2018-04-01 17:50:11 -0400

    Update humanPlayer.h

--- Player/humanPlayer.h
+++ Player/humanPlayer.h
@@ -5,7 +5,7 @@
 
 class HumanPlayer: public Player {
 	public:
-	void move(Pos oldPos, Pos newPos, string permission);
+	void move(Pos oldPos, Pos newPos, string permission) override;
 	HumanPlayer(Color color, Board* B, King* king = NULL);
 	~HumanPlayer();
 };
