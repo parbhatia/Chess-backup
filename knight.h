2760a0c3b72688768ed820811837de78703d9354
Berdem <b2yildiz@uwaterloo.ca>
2018-04-02 02:59:08 -0400

    Update knight.h

--- knight.h
+++ knight.h
@@ -1,8 +1,10 @@
 #ifndef KNIGHT_H
 #define KNIGHT_H
 
+#include <vector>
 #include "piece.h"
 #include "pos.h"
+#include "color.h"
 
 class Knight: public Piece {
 	bool IsLegal(Pos newPos, vector <vector<*Piece>>* pieces) override;
