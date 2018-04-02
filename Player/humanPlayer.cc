a50a7ea6ad8c3a0f05d3bbb6c34c977bd25a96a1
Javidan <cavidan.hasanzade98@gmail.com>
2018-04-01 20:16:26 -0400

    Update humanPlayer.cc

--- Player/humanPlayer.cc
+++ Player/humanPlayer.cc
@@ -1,21 +1,6 @@
 #include "humanPlayer.h"
 #include "pos.h"
 
-static void makeTheMove(Piece* moved, target, Board* b){
-	const Pos mPos = moved->getPos();
-	const Pos tPos = target->getPos();
-	Move* m = new Move(mPos, tPos, target);
-	b->getMoves()->emplace_back(m); //info about move is pushed to moved vec in board
-	b->getPieces()[tPos.row][tPos.col] = moved; //target cell points to moved piece
-	b->getPieces()[mPos.row][mPos.col] = NULL; //freeing old cell
-}
-
-static bool outOfRange(const Pos p) {
-	if (p.row > 7 || p.col > 7) return true;
-	else if (p.row < 0 || p.col < 0) return true;
-return false;
-}
-
 HumanPlayer::void move(Pos oldPos, newPos, string prm){
 	Piece* curPiece = B.getPieces[oldPos.row][oldPos.col];
 	Piece* target = B.getPieces[newPos.row][newPos.col];//piece at newPos
