55731fa822b6f2b0e76c0d68f451fc205575c306
Par Bhatia <parshantx@hotmail.com>
2018-04-02 17:54:11 -0400

    MAJOR CHANGES

---
+++ piecepositions.h
@@ -0,0 +1,40 @@
+#ifndef PIECE_POS
+#define PIECE_POS
+#include "pos.h"
+
+//STANDARD PIECE POSITIONS
+Pos R1={7,0};
+Pos N1={7,1};
+Pos B1={7,2};
+Pos Q={7,3};
+Pos K={7,4};
+Pos B2={7,5};
+Pos N2={7,6};
+Pos R2={7,7};
+Pos P1={6,0};
+Pos P2={6,1};
+Pos P3={6,2};
+Pos P4={6,3};
+Pos P5={6,4};
+Pos P6={6,5};
+Pos P7={6,6};
+Pos P8={6,7};
+
+Pos r1={0,0};
+Pos n1={0,1};
+Pos b1={0,2};
+Pos q={0,3};
+Pos k={0,4};
+Pos b2={0,5};
+Pos n2={0,6};
+Pos r2={0,7};
+Pos p1={1,0};
+Pos p2={1,1};
+Pos p3={1,2};
+Pos p4={1,3};
+Pos p5={1,4};
+Pos p6={1,5};
+Pos p7={1,6};
+Pos p8={1,7};
+
+#endif
